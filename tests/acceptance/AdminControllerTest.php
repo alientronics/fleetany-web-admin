6bb4952924e50548cce2ad287600979a2a95ef6f
Rafael <rafaelpviegas@gmail.com>
2016-07-24 01:44:49 -0300

    Correção testes controller

--- tests/acceptance/AdminControllerTest.php
+++ tests/acceptance/AdminControllerTest.php
@@ -9,68 +9,68 @@ class AdminControllerTest extends AcceptanceTestCase
 
     private function setEloquentMock($method, $return)
     {
-        $mockRepo = \Mockery::mock('Alientronics\FleetanyWebAttributes\Repositories\AttributeRepositoryEloquent');
+        $mockRepo = \Mockery::mock('Alientronics\FleetanyWebAdmin\Repositories\RoleRepositoryEloquent');
         $mockRepo->shouldReceive($method)->andReturn($return);
 
-        $this->app->instance('Alientronics\FleetanyWebAttributes\Repositories\AttributeRepositoryEloquent', $mockRepo);
+        $this->app->instance('Alientronics\FleetanyWebAdmin\Repositories\RoleRepositoryEloquent', $mockRepo);
     }
 
     public function testIndex()
     {
-        $this->setEloquentMock('results', 'entity attributes');
-        $this->get('/attribute')->see('entity attributes');
+        $this->setEloquentMock('results', 'entity roles');
+        $this->get('/role')->see('entity roles');
     }
 
     public function testCreate()
     {
-        $this->get('/attribute/create')->see('vehicle');
+        $this->get('/role/create')->see('vehicle');
     }
 
     public function testEdit()
     {
         $this->setEloquentMock('getKey', (object)['key'=>'entity key']);
-        $this->get('/attribute/1/edit')->see('entity key');
+        $this->get('/role/1/edit')->see('entity key');
     }
 
     public function testUpdateTrue()
     {
         $this->setEloquentMock('updateKey', true);
-        $this->put('/attribute/1')->assertRedirectedTo('attribute', ['message'=>'general.succefullupdate']);
+        $this->put('/role/1')->assertRedirectedTo('role', ['message'=>'general.succefullupdate']);
     }
 
     public function testUpdateFalse()
     {
         $this->setEloquentMock('updateKey', false);
-        $this->put('/attribute/1')->assertRedirectedTo('/');
+        $this->put('/role/1')->assertRedirectedTo('/');
     }
 
     public function testStoreTrue()
     {
         $this->setEloquentMock('createKey', true);
-        $this->post('/attribute')->assertRedirectedTo('attribute', ['message'=>'general.succefullcreate']);
+        $this->post('/role')->assertRedirectedTo('role', ['message'=>'general.succefullcreate']);
     }
 
     public function testStoreFalse()
     {
         $this->setEloquentMock('createKey', false);
-        $this->post('/attribute')->assertRedirectedTo('/');
+        $this->post('/role')->assertRedirectedTo('/');
     }
 
     public function testDestroyTrue()
     {
         $this->setEloquentMock('deleteKey', true);
-        $this->delete('/attribute/1')->assertRedirectedTo('attribute', ['message'=>'general.deletedregister']);
+        $this->delete('/role/1')->assertRedirectedTo('role', ['message'=>'general.deletedregister']);
     }
 
     public function testDestroyFalse()
     {
         $this->setEloquentMock('deleteKey', false);
-        $this->delete('/attribute/1')->assertRedirectedTo('attribute', ['message'=>'general.deletedregistererror']);
+        $this->delete('/role/1')->assertRedirectedTo('role', ['message'=>'general.deletedregistererror']);
     }
 
     public function testDestroy()
     {
         $this->setEloquentMock('deleteKey', true);
-        $this->get('/attribute/destroy/1')->assertRedirectedTo('attribute');
+        $this->get('/role/destroy/1')->assertRedirectedTo('role');
     }
 }
